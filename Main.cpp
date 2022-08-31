5569c9fe9c38d49fdb2f1f6902d800c41c067eeb
fcastillo8 <111403444+fcastillo8@users.noreply.github.com>
2022-08-31 11:34:08 -0400

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "El Tipico Hola Mundo !!" << endl; // Escribe un mensaje en pantalla
+
+	cout << "Escrito desde C++" << endl;
+	cout << "Hola Mundo !!";
+	cout << endl;
+
+	/*
+	 Si no hay errores el programa termina su ejecucion
+	*/
+
+	return 0;
+}
